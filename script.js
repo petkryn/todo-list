ab9f0fdb19f207d40a15c997519f9595a2f213cb
Petro <pkrynytskyi3@gmail.com>
2024-09-29 14:59:24 +0300

    Code improvements

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let taskList = document.querySelector(".todo__tasks-wrapper")
 addTaskButton.addEventListener("click", addTaskHandler);
 
 function createTask(text) {
-    let div = document.createElement("div");
+    const div = document.createElement("div");
     div.classList.add("todo__task");
     div.innerHTML = `
             <span class="todo__task-description">${text}</span>
@@ -13,7 +13,7 @@ function createTask(text) {
                 <button class="todo__task-btn">
                     <img class="todo__img" src="img/pencil.png" alt="correction">
                 </button>
-                <button class="todo__task-btn" onclick="deletetask(event)">
+                <button class="todo__task-btn" onclick="deleteTask(event)">
                     <img class="todo__img" src="img/bin.png" alt="delete">
                 </button>
             </div>
@@ -21,13 +21,17 @@ function createTask(text) {
     return div;
 }
 
-function deletetask(event) {
-    event.target.parentElement.parentElement.parentElement.remove();
+function deleteTask(event) {
+    // event.target.parentElement.parentElement.parentElement.remove();
+    let taskItem = event.target.closest(".todo__tasks-wrapper");
+    if (taskItem) {
+        taskItem.remove();
+    }
 }
 
 function addTaskHandler() {
     if (taskNameInput.value) {
-        let newTask = createTask(taskNameInput.value);
+        const newTask = createTask(taskNameInput.value);
         taskList.append(newTask);
         taskNameInput.value = "";
     } else { 
