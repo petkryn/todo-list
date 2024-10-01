ec80541a5fd87a05bb5ce22e33fd465cf76ff65e
petkryn <140324556+petkryn@users.noreply.github.com>
2024-10-01 15:17:58 +0300

    Remove BTN (#4)
    * Remove BTN
    
    * Code improvements
    
    * Code improvements

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taskNameInput = document.querySelector("#todo-input");
-let addTaskButton = document.querySelector("#add-button");
-let taskList = document.querySelector(".todo__tasks-wrapper")
+const taskNameInput = document.querySelector("#todo-input");
+const addTaskButton = document.querySelector("#add-button");
+const taskList = document.querySelector(".todo__tasks-wrapper")
 
 addTaskButton.addEventListener("click", addTaskHandler);
 
 function createTask(text) {
-    let div = document.createElement("div");
+    const div = document.createElement("div");
     div.classList.add("todo__task");
     div.innerHTML = `
             <span class="todo__task-description">${text}</span>
@@ -13,7 +13,7 @@ function createTask(text) {
                 <button class="todo__task-btn">
                     <img class="todo__img" src="img/pencil.png" alt="correction">
                 </button>
-                <button class="todo__task-btn">
+                <button class="todo__task-btn" onclick="deleteTask(event)">
                     <img class="todo__img" src="img/bin.png" alt="delete">
                 </button>
             </div>
@@ -21,9 +21,16 @@ function createTask(text) {
     return div;
 }
 
+function deleteTask(event) {
+    const taskItem = event.target.closest(".todo__tasks-wrapper");
+    if (taskItem) {
+        taskItem.remove();
+    }
+}
+
 function addTaskHandler() {
     if (taskNameInput.value) {
-        let newTask = createTask(taskNameInput.value);
+        const newTask = createTask(taskNameInput.value);
         taskList.append(newTask);
         taskNameInput.value = "";
     } else { 
